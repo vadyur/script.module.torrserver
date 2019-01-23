1f3f9ec7a7667fe513cfe0df8e283f902ca8c539
Vd <vadyur@csodessa.com>
2019-01-23 12:29:43 +0200

    try parse torrent content

--- lib/torrserve_stream/engine.py
+++ lib/torrserve_stream/engine.py
@@ -51,6 +51,9 @@ class BaseEngine(object):
 
 	def stat(self):
 		return self.request('stat', data={'Hash': self.hash}).json()
+
+	def get(self):
+		return self.request('get', data={'Hash': self.hash}).json()
 		
 	def list(self):
 		return self.request('list').json()
@@ -114,6 +117,7 @@ class Engine(BaseEngine):
 		self.hash = None
 		self.log = log
 		self.success = True
+		self.playable_items = []
 
 		if not self.echo():
 			self.success = False
@@ -137,6 +141,116 @@ class Engine(BaseEngine):
 			self.upload(name, data)
 			self._wait_for_data()
 
+	def _get_playable_items(self):
+
+		if self.playable_items:
+			return self.playable_items
+
+		import os
+		from bencode import bdecode
+		decoded = bdecode(self.data)
+
+		info = decoded['info']
+
+		def info_name():
+			if 'name.utf-8' in info:
+				return info['name.utf-8']
+			else:
+				return info['name']
+
+		def f_path(f):
+			if 'path.utf-8' in f:
+				return f['path.utf-8']
+			else:
+				return f['path']
+
+		def _name(name):
+			try:
+				return name.decode('utf-8')
+			except UnicodeDecodeError:
+				import chardet
+				enc = chardet.detect(name)
+				if enc['confidence'] > 0.5:
+					try:
+						name = name.decode(enc['encoding'])
+					except UnicodeDecodeError:
+						pass
+			return name
+
+		info_name = _name(info_name())
+		try:
+			if 'files' in info:
+				for i, f in enumerate(info['files']):
+					name = _name('/'.join(f_path(f)))
+					name = u'/'.join([info_name, name])
+					size = f['length']
+
+					self.playable_items.append({'index': i, 'name': name, 'size': size})
+			else:
+				self.playable_items = [ {'index': 0, 'name': info_name, 'size': info['length'] } ]
+		except UnicodeDecodeError:
+			return None
+
+		return self.playable_items
+
+	def _magnet2data(self, magnet):
+		self.log('-'*100)
+		self.log('_magnet2data')
+
+		try:
+			import libtorrent as lt
+		except ImportError:
+			self.log('_magnet2data: import error')
+			return None
+
+		import tempfile, sys, shutil
+		from time import sleep
+
+		tempdir = tempfile.mkdtemp()
+		ses = lt.session()
+		params = {
+			'save_path': tempdir,
+			'storage_mode': lt.storage_mode_t(2),
+			'paused': False,
+			'auto_managed': True,
+			'duplicate_is_error': True
+		}
+		handle = lt.add_magnet_uri(ses, magnet, params)
+
+		self.log("Downloading Metadata (this may take a while)")
+		while (not handle.has_metadata()):
+			try:
+				sleep(1)
+				self.log('Wait for data')
+			except KeyboardInterrupt:
+				self.log("Aborting...")
+				ses.pause()
+				self.log("Cleanup dir " + tempdir)
+				shutil.rmtree(tempdir)
+				return None
+		ses.pause()
+		self.log("Done")
+
+		torinfo = handle.get_torrent_info()
+		torfile = lt.create_torrent(torinfo)
+
+		torcontent = lt.bencode(torfile.generate())
+		return torcontent
+
+	def upload(self, name, data):
+		self.data = data
+		BaseEngine.upload(self, name, data)
+
+	def add(self, uri):
+		if uri.startswith('magnet:'):
+			self.data = self._magnet2data(uri)
+		else:
+			r = requests.get(uri)
+			if r.status_code == requests.codes.ok:
+				self.data = r.content
+
+		BaseEngine.add(self, uri)
+
 	def start_preload(self, url):
 		def download_stream():
 			req = requests.get(url, stream=True)
@@ -148,29 +262,20 @@ class Engine(BaseEngine):
 		t.start()
 
 	def id_to_files_index(self, file_id):
-		st = self.stat()
 		ts = self.torrent_stat()
-		index = 0
-		for item in st.get('FileStats') or []:
-			if item['Id'] == file_id:
-				self.log(unicode(item['Path']))
 
-				files_index = 0
-				for fl in ts['Files']:
-					self.log(unicode(fl['Name']))
-					if item['Path'] == fl['Name']:
-						return files_index
-					files_index += 1
+		try:
+			pi = self._get_playable_items()[file_id]
+		except:
+			return file_id
 
-				files_index = 0
-				for fl in ts['Files']:
-					if item['Lenght'] == fl['Size']:
-						return files_index
-					files_index += 1
+		files_index = 0
+		for fl in ts['Files']:
+			self.log(unicode(fl['Name']))
+			if pi['name'] == fl['Name']:
+				return files_index
+			files_index += 1
 
-			index += 1
-
-		self.log('Index not found, just return file_id')
 		return file_id
 
 	def start(self, start_index=None):
@@ -238,7 +343,7 @@ class Engine(BaseEngine):
 		return 0
 
 if __name__ == '__main__':
-	path = 'D:\\book7.torrent'
+	path = 'D:\\test.torrent'
 	file_id = 2
 
 	def log(s):
@@ -249,11 +354,17 @@ if __name__ == '__main__':
 				f.write(s)
 			f.write('\n')
 
-	e = Engine(path=path, host='192.168.1.5', log=log)
+	# host='192.168.1.5'
+	e = Engine(path=path, log=log)
+	#e = Engine(uri='http://rutor.is/download/657889', log=log)
+	#e = Engine(uri='magnet:?xt=urn:btih:a60f1bf7abf47af05ff8bd2b5f33fff65e7d7159&dn=rutor.info_%D0%9C%D0%B0%D0%BD%D0%B8%D1%84%D0%B5%D1%81%D1%82+%2F+%D0%94%D0%B5%D0%BA%D0%BB%D0%B0%D1%80%D0%B0%D1%86%D0%B8%D1%8F+%2F+Manifest+%5B01%D1%8501-11+%D0%B8%D0%B7+18%5D+%282018%29+WEB-DL+720p+%7C+LostFilm&tr=udp://opentor.org:2710&tr=udp://opentor.org:2710&tr=http://retracker.local/announce', log=log)
 
-	play_url = e.play_url(file_id)
+	g = e.get()
+	s = e.stat()
 
-	print(play_url)
+	for file_id in range(0, 8):
+		play_url = e.play_url(file_id)
+		log(play_url)
 
 	
 
