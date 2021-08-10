cd8bf65081d094bfe575c748a98fd1e85e1d9317
vd <vadyur@csodessa.com>
2021-08-10 16:53:59 +0300

    Add Authentication

--- lib/torrserve_stream/settings.py
+++ lib/torrserve_stream/settings.py
@@ -9,6 +9,13 @@ class Settings(object):
         self.host = _addon.getSetting('host')
         self.port = int(_addon.getSetting('port'))
         self.save_in_database = _addon.getSetting('save_in_database') == "true"
+        
+        use_auth = _addon.getSetting('use_auth') == 'true'
+
+        login = _addon.getSetting('login')
+        passw = _addon.getSetting('passw')
+
+        self.auth = ( login, passw ) if use_auth else None
 
 if __name__ == '__main__':
     xbmc.log('open settings')
